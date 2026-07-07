ba0d097d33bf540387cef8d558dab0658a63384f
tarnasi <shahryar.tarnasi@gmail.com>
2026-07-07 14:50:30 +0400

    add main page route

--- main.py
+++ main.py
@@ -11,3 +11,12 @@ app.include_router(register.router, prefix="/api/auth")
 app.include_router(login.router, prefix="/api/auth")
 app.include_router(profile.router, prefix="/api/auth")
 app.include_router(logout.router, prefix="/api/auth")
+
+@app.get('/')
+def main_page():
+    return {
+        "prefix": "api/auth",
+        "title": "enterance",
+        "owner": "shahriyar tarnasi",
+        "contactMe": "shahryar.tarnasi@gmail.com"
+    }
